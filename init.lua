57155eef0a1696d145e5e2478c0712920539b03c
Reimar Rosas <reimar@reimarrosas.me>
2024-09-02 14:40:37 -0400

    Change: Added python on formatting

--- init.lua
+++ init.lua
@@ -699,8 +699,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        ocaml = { 'ocamlformat' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
