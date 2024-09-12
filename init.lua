ce58993bf7c7b13019ab04ee9f463417e8201236
Reimar Rosas <reimar@reimarrosas.me>
2024-09-12 17:58:51 -0400

    Update: changed tabstops to 4 instead of the default 8

--- init.lua
+++ init.lua
@@ -98,6 +98,11 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Tabstops
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
